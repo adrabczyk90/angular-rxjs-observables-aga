44d116771afec0ea2bd0f1894cf130c211a566ce
adrabczyk90 <adrabczyk90@gmail.com>
2020-10-25 18:52:58 +0000

    learning basics of RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component, VERSION } from '@angular/core';
-import { of, from } from 'rxjs';
-
+import { Component, VERSION } from "@angular/core";
+import { of, from } from "rxjs";
+import { map, tap, take } from "rxjs/operators";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
 
-  ngOnInit(){
+  ngOnInit() {
     of(2, 4, 6, 8).subscribe(console.log);
-  
 
-  from([20, 15, 10, 5]).subscribe(
-    item => console.log(`resulting item .. ${item}`),
-    err => console.log(`error occured .. ${err}`),
-    () => console.log('complete'),
-  );
+    from([20, 15, 10, 5])
+      .pipe(
+        tap(item => console.log(`emitted item .. ${item}`)),
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if(item === 0){
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3)
+
+      )
+      .subscribe(
+        item => console.log(`resulting item .. ${item}`),
+        err => console.log(`error occured .. ${err}`),
+        () => console.log("complete")
+      );
   }
 }
